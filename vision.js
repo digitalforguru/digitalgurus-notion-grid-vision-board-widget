ccf7169308178b56ac2cfeb05a1f8da9c67cb259
digitalforguru <digitalforguru@gmail.com>
2026-05-05 16:40:00 -0600

    Update vision.js

--- vision.js
+++ vision.js
@@ -1,4 +1,3 @@
-any chance you can apply these changes to my js and return me the full updated polished file without changing anything else? 
 const supabaseUrl = "https://fcajhwkmsyztkvyzjhkl.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjYWpod2ttc3l6dGt2eXpqaGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTE0ODIsImV4cCI6MjA5MzU2NzQ4Mn0.fbdUZmZbDCi5IhTvtg1AwjIxXefetuk912YEwjJNqbI";
 
